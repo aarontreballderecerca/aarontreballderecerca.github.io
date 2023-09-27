656283ec4aa0aadc6279abc92cd4fcfd989b3a1f
aarontreballderecerca <128906563+aarontreballderecerca@users.noreply.github.com>
2023-09-27 20:07:30 +0200

    Update rec.js

--- js/rec.js
+++ js/rec.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const appliances = {
         Nevera: 100,
         tv: 50,
-        "Lavadora": 500,
+        Lavadora: 500,
         Microondas: 800,
         Tostadora: 700,
         "Cafetera": 900,
